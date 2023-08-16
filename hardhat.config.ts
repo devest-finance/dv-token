2cb60abc5652383099ab216f49e94de2b42416b9
sk263 <simon.kosmrl@gmail.com>
2023-08-16 10:45:47 +0200

    zkSync deployment script

---
+++ hardhat.config.ts
@@ -0,0 +1,31 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+
+import "@matterlabs/hardhat-zksync-verify";
+
+// dynamically changes endpoints for local tests
+const zkSyncTestnet = {
+        url: "https://zksync2-testnet.zksync.dev",
+        ethNetwork: "goerli",
+        zksync: true,
+        // contract verification endpoint
+        verifyURL:
+          "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
+      };
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "zkSyncTestnet",
+  networks: {
+    hardhat: {
+      zksync: false,
+    },
+    zkSyncTestnet,
+  },
+  solidity: {
+    version: "0.8.12",
+  },
+};
+
+export default config;
